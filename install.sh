453fd2fa523a79b86f3d659c88b9fe64a0a7d9d6
Andrea Rabbaglietti <rabbagliettiandrea@gmail.com>
2023-01-06 12:32:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+YUM_CMD=$(which yum)
+APT_GET_CMD=$(which apt-get)
+
+if [[ ! -z $YUM_CMD ]]; then
+    yum install -y dialog
+elif [[ ! -z $APT_GET_CMD ]]; then
+    apt-get install -y dialog
+else
+    echo "error can't install package dialog"
+    exit 1;
+fi
+
 rm -f /usr/local/bin/nssh
 curl -s https://raw.githubusercontent.com/rabbagliettiandrea/nssh/main/nssh > /usr/local/bin/nssh
 chmod +x /usr/local/bin/nssh
